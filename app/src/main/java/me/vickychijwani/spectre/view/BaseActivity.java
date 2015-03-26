08847a9c198b0207d55de4d2465f2b7288ca2e84
Vicky Chijwani <vickychijwani@gmail.com>
2015-03-27 00:31:11 +0530

    Save / restore UI state correctly in PostViewActivity.

--- app/src/main/java/me/vickychijwani/spectre/view/BaseActivity.java
+++ app/src/main/java/me/vickychijwani/spectre/view/BaseActivity.java
@@ -1,8 +1,11 @@
 package me.vickychijwani.spectre.view;
 
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
 import java.util.List;
@@ -46,6 +49,37 @@ public abstract class BaseActivity extends ActionBarActivity {
             }
         }
         super.onBackPressed();
+    }
+
+    /**
+     * Add a {@link android.support.v4.app.Fragment} of the given type {@link T}. NOTE: this method
+     * is only designed to work with Fragments that have a zero-argument static factory method named
+     * newInstance().
+     *
+     * @param type      the class of the Fragment to add
+     * @param container the ID of the view container in which to add this Fragment's view root
+     * @param tag       the tag to assign to this Fragment, used for retrieval later
+     * @param <T>       the type of the Fragment
+     * @return a non-null instance of type {@link T} (could be a new instance or one already
+     *                  attached to the activity)
+     */
+    @SuppressWarnings("unchecked")
+    @NonNull
+    public <T extends Fragment> T addFragment(Class<T> type, @IdRes int container, String tag) {
+        T fragment = (T) getSupportFragmentManager().findFragmentByTag(tag);
+        if (fragment == null) {
+            try {
+                fragment = type.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                Log.wtf("WTF", "Given Fragment class does not have a zero-argument static factory" +
+                        "method named newInstance(), as required by this method");
+            }
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(container, fragment, tag)
+                    .commit();
+        }
+        return fragment;
     }
 
 }
--- app/src/main/java/me/vickychijwani/spectre/view/PostViewActivity.java
+++ app/src/main/java/me/vickychijwani/spectre/view/PostViewActivity.java
@@ -1,6 +1,7 @@
 package me.vickychijwani.spectre.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,12 +21,17 @@ public class PostViewActivity extends BaseActivity implements
 
     private static final String TAG = "PostViewActivity";
 
+    private static final String TAG_VIEW_FRAGMENT = "tag:view_fragment";
+    private static final String TAG_EDIT_FRAGMENT = "tag:edit_fragment";
+    private static final String KEY_IS_PREVIEW_VISIBLE = "key:is_preview_visible";
+
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
 
     private Post mPost;
     private PostViewFragment mPostViewFragment;
     private PostEditFragment mPostEditFragment;
+    private boolean mIsPreviewVisible = false;
     private View.OnClickListener mUpClickListener;
 
     @Override
@@ -38,33 +44,43 @@ public class PostViewActivity extends BaseActivity implements
 
         mPost = Parcels.unwrap(getIntent().getExtras().getParcelable(BundleKeys.POST));
 
-        mPostViewFragment = PostViewFragment.newInstance();
-        mPostEditFragment = PostEditFragment.newInstance();
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.fragment_container, mPostViewFragment)
-                .add(R.id.fragment_container, mPostEditFragment)
-                .commit();
-
         mUpClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 NavUtils.navigateUpFromSameTask(PostViewActivity.this);
             }
         };
+
+        mPostViewFragment = addFragment(PostViewFragment.class, R.id.fragment_container, TAG_VIEW_FRAGMENT);
+        mPostEditFragment = addFragment(PostEditFragment.class, R.id.fragment_container, TAG_EDIT_FRAGMENT);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState.getBoolean(KEY_IS_PREVIEW_VISIBLE, false)) {
+            onPreviewClicked();
+        }
     }
 
     @Override
     public void onPreviewClicked() {
         mPostEditFragment.hide();
         mPostViewFragment.show();
+        mIsPreviewVisible = true;
     }
 
     @Override
     public void onEditClicked() {
         mPostViewFragment.hide();
         mPostEditFragment.show();
+        mIsPreviewVisible = false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IS_PREVIEW_VISIBLE, mIsPreviewVisible);
     }
 
     public void setNavigationItem(int iconResId, View.OnClickListener clickListener) {
