71e280c59621fba7dd5ad0abc60c3bc7ac8761eb
Vicky Chijwani <vickychijwani@gmail.com>
2015-05-16 05:09:12 +0530

    Log BaseFragment lifecycle methods to Crashlytics and Logcat.

--- app/src/main/java/me/vickychijwani/spectre/view/fragments/BaseFragment.java
+++ app/src/main/java/me/vickychijwani/spectre/view/fragments/BaseFragment.java
@@ -1,15 +1,24 @@
 package me.vickychijwani.spectre.view.fragments;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.crashlytics.android.Crashlytics;
 import com.squareup.otto.Bus;
 import com.squareup.picasso.Picasso;
 
 import me.vickychijwani.spectre.SpectreApplication;
 import me.vickychijwani.spectre.event.BusProvider;
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+    private static final String TAG = "BaseFragment";
 
     protected Bus getBus() {
         return BusProvider.getBus();
@@ -19,16 +28,62 @@ public class BaseFragment extends Fragment {
         return SpectreApplication.getInstance().getPicasso();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onCreateView()");
+        return null;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onStart()");
+    }
+
     @Override
     public void onResume() {
         super.onResume();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onResume()");
         getBus().register(this);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onAttach()");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onDetach()");
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onPause()");
         getBus().unregister(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onStop()");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Crashlytics.log(Log.DEBUG, TAG, this.getClass().getName() + "#onDestroy()");
     }
 
     /**
