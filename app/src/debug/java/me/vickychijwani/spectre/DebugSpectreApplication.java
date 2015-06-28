3a75409f263df89435acfe7efe2d550169f579ae
Vicky Chijwani <vickychijwani@gmail.com>
2015-06-29 03:06:27 +0530

    Fix #41: Test with https.
    Tested with self-signed certificate.

--- app/src/debug/java/me/vickychijwani/spectre/DebugSpectreApplication.java
+++ app/src/debug/java/me/vickychijwani/spectre/DebugSpectreApplication.java
@@ -6,6 +6,13 @@ import com.squareup.leakcanary.LeakCanary;
 import com.squareup.okhttp.OkHttpClient;
 import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class DebugSpectreApplication extends SpectreApplication {
 
     @Override
@@ -27,7 +34,42 @@ public class DebugSpectreApplication extends SpectreApplication {
     protected OkHttpClient getOkHttpClient() {
         OkHttpClient client = super.getOkHttpClient();
         client.networkInterceptors().add(new StethoInterceptor());
+
+        // trust all SSL certs, for TESTING ONLY!
+        client.setHostnameVerifier((hostname, session) -> true);
+        client.setSslSocketFactory(getUnsafeSslSocketFactory());
+
         return client;
+    }
+
+    private SSLSocketFactory getUnsafeSslSocketFactory() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] { new TrustEveryoneManager() };
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            return sslContext.getSocketFactory();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // private stuff
+    private static class TrustEveryoneManager implements X509TrustManager {
+        @Override
+        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                throws CertificateException {}
+
+        @Override
+        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+                throws CertificateException {}
+
+        @Override
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
     }
 
 }
