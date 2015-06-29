1c84977821b28724cd28bcaf1af2df101564a4c5
Vicky Chijwani <vickychijwani@gmail.com>
2015-06-30 01:09:09 +0530

    Fix errorType field name in ApiError model (belongs with commit be0b8dc).

--- app/src/main/java/me/vickychijwani/spectre/model/ApiError.java
+++ app/src/main/java/me/vickychijwani/spectre/model/ApiError.java
@@ -1,8 +1,14 @@
 package me.vickychijwani.spectre.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ApiError {
 
     public String message;          // meant for humans
+
+    // unfortunately there is a bug in the field name on the API side, it is "errorType" rather than
+    // "error_type", hence the custom @SerializedName
+    @SerializedName("errorType")
     public String errorType;        // meant for machines: "UnauthorizedError", etc.
 
 }
