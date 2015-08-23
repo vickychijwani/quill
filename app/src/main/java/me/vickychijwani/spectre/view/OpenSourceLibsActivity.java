b35d40681b9d45e8f745716431d32b4cf5fa5c4b
Vicky Chijwani <vickychijwani@gmail.com>
2015-08-24 00:15:07 +0700

    Update library credits.

--- app/src/main/java/me/vickychijwani/spectre/view/OpenSourceLibsActivity.java
+++ app/src/main/java/me/vickychijwani/spectre/view/OpenSourceLibsActivity.java
@@ -38,10 +38,12 @@ public class OpenSourceLibsActivity extends BaseActivity {
             new Library("Realm", "Realm Inc.", "http://realm.io/"),
             new Library("Retrofit", "Square Inc.", "http://square.github.io/retrofit/"),
             new Library("RxAndroid", "ReactiveX", "https://github.com/ReactiveX/RxAndroid"),
+            new Library("RxLifecycle", "Trello Inc.", "https://github.com/trello/RxLifecycle"),
             new Library("Showdown", "ShowdownJS and Hannah Wolfe", "https://github.com/ErisDS/showdown/"),
             new Library("Slugify", "Danny Trunk", "https://github.com/slugify/slugify"),
             new Library("Stetho", "Facebook Inc.", "http://facebook.github.io/stetho/"),
-            new Library("TokenAutoComplete", "Splitwise Inc.", "https://github.com/splitwise/TokenAutoComplete")
+            new Library("TokenAutoComplete", "Splitwise Inc.", "https://github.com/splitwise/TokenAutoComplete"),
+            new Library("Typekit", "Hien Ngo", "https://github.com/tsengvn/typekit")
     );
 
     private LibsAdapter mLibsAdapter;
