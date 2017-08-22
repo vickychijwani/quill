d0bacee68d4836e22aa439c7b70b4e57044cb923
Vicky Chijwani <vickychijwani@gmail.com>
2017-08-23 03:57:57 +0530

    [Ghost 1.0] Link to legacy app version for Ghost 0.x on error screen.

--- app/src/main/java/me/vickychijwani/spectre/view/fragments/GhostV0ErrorFragment.java
+++ app/src/main/java/me/vickychijwani/spectre/view/fragments/GhostV0ErrorFragment.java
@@ -18,6 +18,7 @@ public class GhostV0ErrorFragment extends BaseFragment {
     @Bind(R.id.error_title)     TextView mErrorTitle;
     @Bind(R.id.error_content)   TextView mErrorContent;
     @Bind(R.id.error_tip)       TextView mErrorTip;
+    @Bind(R.id.error_legacy)    TextView mErrorLegacy;
 
     public static GhostV0ErrorFragment newInstance() {
         GhostV0ErrorFragment fragment = new GhostV0ErrorFragment();
@@ -33,12 +34,15 @@ public class GhostV0ErrorFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ghost_v0_error, container, false);
         ButterKnife.bind(this, view);
+        String appName = getString(R.string.app_name);
         mErrorTitle.setText(Html.fromHtml(mErrorTitle.getText().toString()));
         mErrorContent.setText(Html.fromHtml(mErrorContent.getText().toString()));
         mErrorTip.setText(Html.fromHtml(mErrorTip.getText().toString()));
+        mErrorLegacy.setText(Html.fromHtml(String.format(mErrorLegacy.getText().toString(), appName)));
         mErrorTitle.setMovementMethod(LinkMovementMethod.getInstance());
         mErrorContent.setMovementMethod(LinkMovementMethod.getInstance());
         mErrorTip.setMovementMethod(LinkMovementMethod.getInstance());
+        mErrorLegacy.setMovementMethod(LinkMovementMethod.getInstance());
         return view;
     }
 
